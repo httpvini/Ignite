e462c423eeff0e73800766ab57568f43905fad4e
Vinicius Oliveira <viniciusoliveira@Mac-mini-de-Vinicius.local>
2021-05-27 02:05:30 -0300

    Disponibiliza endpoint para cadastro de nova conta, respeitando regra de negócio de apenas um cpf por conta cadastrada.

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -1,5 +1,45 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (req, res)=> {
+    const { cpf, name } = req.body;
+
+    if(isThereACostumer(cpf)){
+        return res.status(400).json(
+            {error: 'Customer already exists!'}
+        );
+    }
+
+    saveCustomer(cpf, name);
+
+    return res.status(201).send();
+});
+
+const saveCustomer = (cpf, name) => {
+    customers.push({
+        cpf,
+        name,
+        id:uuidv4(),
+        statement: []
+    });
+}
+
+const isThereACostumer = (cpf) => {
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return true;
+    }
+
+    return false
+}
 
 app.listen(5000, ()=>{
     console.log('Magic shit going on!');
