2686e2c96a65997f0639f6b7e730315cd705b850
Vinicius Oliveira <viniciusoliveira@Mac-mini-de-Vinicius.local>
2021-05-28 00:15:11 -0300

    Cria endpoint para busca de extrato e valida se a conta existe. Aplicação do prettier nos arquivos js

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -1,46 +1,59 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 app.use(express.json());
 
 const customers = [];
 
-app.post('/account', (req, res)=> {
-    const { cpf, name } = req.body;
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
 
-    if(isThereACostumer(cpf)){
-        return res.status(400).json(
-            {error: 'Customer already exists!'}
-        );
-    }
+  if (isThereACostumer(cpf)) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
 
-    saveCustomer(cpf, name);
+  saveCustomer(cpf, name);
 
-    return res.status(201).send();
+  return res.status(201).send();
+});
+
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+  const customer = findCustomer(cpf);
+
+  if (!customer) {
+    res.status(404).json({ error: "Customer not found!" });
+  }
+
+  res.status(200).json(customer.statement);
 });
 
 const saveCustomer = (cpf, name) => {
-    customers.push({
-        cpf,
-        name,
-        id:uuidv4(),
-        statement: []
-    });
-}
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+};
+
+const findCustomer = (cpf) => {
+  return customers.find((customer) => customer.cpf === cpf);
+};
 
 const isThereACostumer = (cpf) => {
-    const customerAlreadyExists = customers.some(
-        (customer) => customer.cpf === cpf
-    );
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-    if(customerAlreadyExists) {
-        return true;
-    }
+  if (customerAlreadyExists) {
+    return true;
+  }
 
-    return false
-}
+  return false;
+};
 
-app.listen(5000, ()=>{
-    console.log('Magic shit going on!');
+app.listen(5000, () => {
+  console.log("Magic shit going on!");
 });
--- node-fundamentals/src/index.js
+++ node-fundamentals/src/index.js
@@ -1,40 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const CourseService = require('./services/courseService')
+const CourseService = require("./services/courseService");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/courses', (req, res)=>{
-    const courses = CourseService.getCourses();
-    res.status(200).json(courses);
-})
+app.get("/courses", (req, res) => {
+  const courses = CourseService.getCourses();
+  res.status(200).json(courses);
+});
 
-app.post('/courses', (req, res)=> {
-    const response = CourseService.save(req.body.course);
-    res.status(201).json(response);
-})
+app.post("/courses", (req, res) => {
+  const response = CourseService.save(req.body.course);
+  res.status(201).json(response);
+});
 
-app.put('/courses/:course', (req, res)=>{
-    const oldCourse = req.params.course;
-    const newCourse = req.body.newCourse;
-    const response = CourseService.update(oldCourse, newCourse);
-    res.status(200).json(response);
-})
+app.put("/courses/:course", (req, res) => {
+  const oldCourse = req.params.course;
+  const newCourse = req.body.newCourse;
+  const response = CourseService.update(oldCourse, newCourse);
+  res.status(200).json(response);
+});
 
-app.patch('/courses/:course', (req, res)=>{
-    const oldCourse = req.params.course;
-    const newCourse = req.body.newCourse;
-    const response = CourseService.update(oldCourse, newCourse);
-    res.status(200).json(response);
-})
+app.patch("/courses/:course", (req, res) => {
+  const oldCourse = req.params.course;
+  const newCourse = req.body.newCourse;
+  const response = CourseService.update(oldCourse, newCourse);
+  res.status(200).json(response);
+});
 
-app.delete('/courses/:course', (req, res)=>{
-    const course = req.params.course;
-    const response = CourseService.delete(course)
-    res.status(200).json(response);
-})
+app.delete("/courses/:course", (req, res) => {
+  const course = req.params.course;
+  const response = CourseService.delete(course);
+  res.status(200).json(response);
+});
 
-app.listen(5000, ()=>{
-    console.log("MAGIC FUCKING HAPPENING!!!!!!!")
+app.listen(5000, () => {
+  console.log("MAGIC FUCKING HAPPENING!!!!!!!");
 });
